ce1f9303c93d528e1a0db045d4f2c513747937e8
Malisa Vucinic <malishav@gmail.com>
2019-06-06 12:32:35 +0200

    Align with additions to the radio, uart APIs.

--- bsp/boards/remote/debugpins.c
+++ bsp/boards/remote/debugpins.c
@@ -8,24 +8,25 @@
 #include <headers/hw_memmap.h>
 #include <headers/hw_types.h>
 
-#include "debugpins.h"
-#include "gpio.h"
+#include <source/gpio.h>
+
 #include "board.h"
+#include "debugpins.h"
 
 //=========================== defines =========================================
-// Board dbPINS defines, currently there's no pins assigned so ignore the above,
-// or define yourself based on your needs
-
+// Board dbPINS defines
 #define BSP_PINA_BASE           GPIO_A_BASE
+#define BSP_PIND_BASE           GPIO_D_BASE
 
-#define BSP_PINA_4              GPIO_PIN_4
-#define BSP_PINA_5              GPIO_PIN_5
+#define BSP_PINA_2              GPIO_PIN_2      //!< PA2 -- radio       AD4/DIO4
+#define BSP_PINA_3              GPIO_PIN_3      //!< PA3 -- isruarttx   CTS/DIO7
+#define BSP_PINA_4              GPIO_PIN_4      //!< PA4 -- frame       AD5/DIO5
+#define BSP_PINA_5              GPIO_PIN_5      //!< PA5 -- isr         RTS/AD6/DIO6
+#define BSP_PINA_6              GPIO_PIN_6      //!< PA6 -- isruartrx   ON/SLEEP
 
-#define BSP_PIND_3              GPIO_PIN_3
-#define BSP_PIND_2              GPIO_PIN_2
-#define BSP_PIND_1              GPIO_PIN_1
-#define BSP_PIND_0              GPIO_PIN_0
-
+#define BSP_PIND_3              GPIO_PIN_3      //!< PD3 -- slot        AD0/DIO0
+#define BSP_PIND_2              GPIO_PIN_2      //!< PD2 -- fsm         AD1/DIO1
+#define BSP_PIND_1              GPIO_PIN_1      //!< PD1 -- task        AD2/DIO2
 //=========================== variables =======================================
 
 //=========================== prototypes ======================================
@@ -34,68 +35,100 @@ void bspDBpinToggle(uint32_t base,uint8_t ui8Pin);
 
 //=========================== public ==========================================
 
-void debugpins_init() {
-   /* FIXME */
+void debugpins_init(void) {
+    GPIOPinTypeGPIOOutput(BSP_PINA_BASE, BSP_PINA_2 | BSP_PINA_4 | BSP_PINA_5);
+    GPIOPinTypeGPIOOutput(BSP_PIND_BASE, BSP_PIND_3 | BSP_PIND_2 | BSP_PIND_1);
+
+    GPIOPinWrite(BSP_PINA_BASE, (BSP_PINA_2 | BSP_PINA_3 | BSP_PINA_4 | BSP_PINA_5 | BSP_PINA_6), 0x00);
+    GPIOPinWrite(BSP_PIND_BASE, (BSP_PIND_3 | BSP_PIND_2 | BSP_PIND_1), 0);
 }
 
-void debugpins_frame_toggle() {
-   /* FIXME */
+// PA4
+void debugpins_frame_toggle(void) {
+    bspDBpinToggle(BSP_PINA_BASE, BSP_PINA_4);
 }
-void debugpins_frame_clr() {
-   /* FIXME */
+void debugpins_frame_clr(void) {
+    GPIOPinWrite(BSP_PINA_BASE, BSP_PINA_4, 0);
 }
-void debugpins_frame_set() {
-   /* FIXME */
-}
-
-void debugpins_slot_toggle() {
-   /* FIXME */
-}
-void debugpins_slot_clr() {
-   /* FIXME */
-}
-void debugpins_slot_set() {
-   /* FIXME */
+void debugpins_frame_set(void) {
+    GPIOPinWrite(BSP_PINA_BASE, BSP_PINA_4, BSP_PINA_4);
 }
 
-void debugpins_fsm_toggle() {
-   /* FIXME */
+// PD3
+void debugpins_slot_toggle(void) {
+    bspDBpinToggle(BSP_PIND_BASE, BSP_PIND_3);
 }
-void debugpins_fsm_clr() {
-   /* FIXME */
+void debugpins_slot_clr(void) {
+    GPIOPinWrite(BSP_PIND_BASE, BSP_PIND_3, 0);
 }
-void debugpins_fsm_set() {
-   /* FIXME */
-}
-
-void debugpins_task_toggle() {
-   /* FIXME */
-}
-void debugpins_task_clr() {
-   /* FIXME */
-}
-void debugpins_task_set() {
-   /* FIXME */
+void debugpins_slot_set(void) {
+    GPIOPinWrite(BSP_PIND_BASE, BSP_PIND_3, BSP_PIND_3);
 }
 
-void debugpins_isr_toggle() {
-   /* FIXME */
+// PD2
+void debugpins_fsm_toggle(void) {
+    bspDBpinToggle(BSP_PIND_BASE, BSP_PIND_2);
 }
-void debugpins_isr_clr() {
-   /* FIXME */
+void debugpins_fsm_clr(void) {
+    GPIOPinWrite(BSP_PIND_BASE, BSP_PIND_2, 0);
 }
-void debugpins_isr_set() {
-   /* FIXME */
+void debugpins_fsm_set(void) {
+    GPIOPinWrite(BSP_PIND_BASE, BSP_PIND_2, BSP_PIND_2);
 }
 
-void debugpins_radio_toggle() {
-   /* FIXME */
+// PD1
+void debugpins_task_toggle(void) {
+    bspDBpinToggle(BSP_PIND_BASE,BSP_PIND_1);
 }
-void debugpins_radio_clr() {
-   /* FIXME */
+void debugpins_task_clr(void) {
+    GPIOPinWrite(BSP_PIND_BASE, BSP_PIND_1, 0);
 }
-void debugpins_radio_set() {
-   /* FIXME */
+void debugpins_task_set(void) {
+    GPIOPinWrite(BSP_PIND_BASE, BSP_PIND_1, BSP_PIND_1);
+}
+
+// PA3 isruarttx
+void debugpins_isruarttx_toggle(void) {
+    bspDBpinToggle(BSP_PINA_BASE, BSP_PINA_3);
+}
+void debugpins_isruarttx_clr(void) {
+    GPIOPinWrite(BSP_PINA_BASE, BSP_PINA_3, 0);
+}
+void debugpins_isruarttx_set(void) {
+    GPIOPinWrite(BSP_PINA_BASE, BSP_PINA_3, BSP_PINA_3);
+}
+
+// PA6 isruartrx
+void debugpins_isruartrx_toggle(void) {
+    bspDBpinToggle(BSP_PINA_BASE, BSP_PINA_6);
+}
+void debugpins_isruartrx_clr(void) {
+    GPIOPinWrite(BSP_PINA_BASE, BSP_PINA_6, 0);
+}
+void debugpins_isruartrx_set(void) {
+    GPIOPinWrite(BSP_PINA_BASE, BSP_PINA_6, BSP_PINA_6);
+}
+
+// PA5
+void debugpins_isr_toggle(void) {
+    bspDBpinToggle(BSP_PINA_BASE, BSP_PINA_5);
+}
+void debugpins_isr_clr(void) {
+    GPIOPinWrite(BSP_PINA_BASE, BSP_PINA_5, 0);
+}
+void debugpins_isr_set(void) {
+    GPIOPinWrite(BSP_PINA_BASE, BSP_PINA_5, BSP_PINA_5);
+}
+
+// PD0
+void debugpins_radio_toggle(void) {
+    bspDBpinToggle(BSP_PINA_BASE, BSP_PINA_2);
+}
+void debugpins_radio_clr(void) {
+    GPIOPinWrite(BSP_PINA_BASE, BSP_PINA_2, 0);
+}
+void debugpins_radio_set(void) {
+    GPIOPinWrite(BSP_PINA_BASE, BSP_PINA_2, BSP_PINA_2);
 }
 
 //------------ private ------------//
